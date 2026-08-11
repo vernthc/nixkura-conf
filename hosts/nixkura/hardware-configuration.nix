26540621985ce1f62c7dd3fe4d040b313f408c73
vernthc <devvmlll@gmail.com>
2026-08-11 20:07:54 +0300

    changed EFI file UUID

--- hosts/nixkura/hardware-configuration.nix
+++ hosts/nixkura/hardware-configuration.nix
@@ -19,7 +19,7 @@
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/EEAE-0D92";
+    { device = "/dev/disk/by-uuid/6D5F-2B1B";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
